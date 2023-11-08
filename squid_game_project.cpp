0686f5932fd952203ba9e8c1238f7ffd4942b564
kevto <ktoyle@students.kennesaw.edu>
2023-11-08 16:10:53 -0500

    added stuff for buzzer

--- squid_game_project.cpp
+++ squid_game_project.cpp
@@ -1,22 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
+int greenledpin=9; //initialize pin 9
+int redledpin=11;// initialize pin 11
+int buzzer=8;// select digital IO pin for the buzzer
+int buzzerPin=0;// initialize analog pin 0
+
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(greenledpin, OUTPUT);// set green LED pin as "output"
-  pinMode(redledpin,OUTPUT);// set red LED pin as “output”
+  
 
-  int result = myFunction(2, 3); //hi
+    Serial.begin(9600);// set baud rate at 9600
+    pinMode(buzzer,OUTPUT);// set digital IO pin pattern, OUTPUT to be output(pin 8)
+    pinMode(greenledpin, OUTPUT);// set green LED pin as "output"
+    pinMode(redledpin,OUTPUT);// set red LED pin as “output”
+
+  int result = myFunction(2, 3); 
 }
 
 void loop() {
-  // I made changes
-  // put your main code here, to run repeatedly:
+
+      val = analogRead(buzzerPin); // read the analog value from the sensor and assign it to val
+      buzzerFreq = map(val, 0, 1023, 60, 10000);
+      buzzerPeriod = 1e6 / buzzerFreq;
+      halfPeriod = buzzerPeriod /2;
+
+      for(int i = 0; i < 200; i++){
+        digitalWrite(buzzer, HIGH);// sound
+        delayMicroseconds(buzzerPeriod);//delay1ms
+        digitalWrite(buzzer,LOW);//not sound
+        delayMicroseconds(buzzerPeriod);//ms delay  
+      }
+        Serial.print("Frequency is ");
+        Serial.println(buzzerFreq);
+
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
