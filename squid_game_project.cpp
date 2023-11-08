970b72cd9b41d729e7462ca0325ca85ce2747cca
kevto <ktoyle@students.kennesaw.edu>
2023-11-08 15:59:25 -0500

    added stuff for red and green led

--- squid_game_project.cpp
+++ squid_game_project.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 
+int greenledpin=9; //initialize pin 9
+int redledpin=11;// initialize pin 11
+
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(greenledpin, OUTPUT);// set green LED pin as "output"
+  pinMode(redledpin,OUTPUT);// set red LED pin as “output”
+
   int result = myFunction(2, 3); //hi
 }
 
-void loop() {\
+void loop() {
   // I made changes
   // put your main code here, to run repeatedly:
 }
