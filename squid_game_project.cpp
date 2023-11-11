866626b838f46186d3d092f1868a2af9c1516cc0
kevto <ktoyle@students.kennesaw.edu>
2023-11-10 20:34:07 -0500

    setup built in other file to get rid of errors

--- squid_game_project.cpp
+++ squid_game_project.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h> 
 #include <LiquidCrystal.h> // include the library code
+#include <Servo.h>
 
 //analog pins///////////////////////////////////////////////////////////////////
-int buzzerPin=0;// initialize analog pin 0 for buzzer
-int potentiometer =1; //initialize analog pin 1 for potentiometer
+
+int potentiometer =0; //initialize analog pin 1 for potentiometer
 
 
 //digital pins///////////////////////////////////////////////////////////////////////
@@ -12,21 +13,29 @@ int echoPin = 2;    // ECHO pin (ultrasonic sensor)
 int trigPin = 3;    // TRIG pin (ultrasonic sensor)
 int greenledpin=4; //initialize pin 4
 int redledpin=5;// initialize pin 5
-int buzzer=6;// select digital IO pin for the buzzer
+int buzzerPin=6;// select digital IO pin for the buzzer
 int firstServo = 13; //first servor motor that has range sensor on top
 int secondServo =22;//2nd servor motor that sweeps field when a player loses
 int buttonpin=18; //button that starts the game
 
+///Buzzer setup///////////////////////////////////////////////////////////////////
+int buzzerFreq;
+int buzzerPeriod;
+int buzzerVal;
+
+
 ///LCD Pin set up/////////////////////////////////////////////////////////////////////////////
 const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12; // initialize the library by associating any needed LCD interface pin
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7); // with the arduino pin number it is connected to
-
+int potVal = 0;
 /// 1st servo variable set up//////////////////////////////////////////////////////////////////////
+int firstMotorAngle;
 int firstServoVal;// define val for 1st servo motor 
 int firstServoAngle; //define val that holds angle for 1st servo motor
 Servo myFirstServo; //defines 1st servo variable name
 
 ///2nd servo variable set up////////////////////////////////////////////////////////////
+int secondMotorAngle;
 int secondServoVal;// define val for 1st servo motor 
 int secondServoAngle; //define val that holds angle for 1st servo motor
 Servo mySecondServo; //defines 1st servo variable name
@@ -59,8 +68,8 @@ void setup() {
     mySecondServo.attach(secondServo);
 
 
-    pinMode(buttonPin, INPUT); //set button pin as input
-    pinMode(buzzer,OUTPUT);// set digital IO pin pattern, OUTPUT to be output(pin 8)
+    
+    pinMode(buzzerPin,OUTPUT);// set digital IO pin pattern, OUTPUT to be output(pin 8)
     pinMode(greenledpin, OUTPUT);// set green LED pin as "output"
     pinMode(redledpin,OUTPUT);// set red LED pin as “output”
 
@@ -74,25 +83,25 @@ void setup() {
 void loop() {
 
 ////LCD PART////////////////////////////////////////////////////////////////////////
-      val = analogRead(potpin); 
+      potVal = analogRead(potentiometer); 
       lcd.clear();
       lcd.setCursor(0,0);
 
-    if(val == 0){
+    if(potVal == 0){
       lcd.print("Water Level:Dry");
     }
 
 ////Buzzer Part///////////////////////////////////////////////////////////////////////////////////////////////////
 
-      val = analogRead(buzzerPin); // read the analog value from the sensor and assign it to val
-      buzzerFreq = map(val, 0, 1023, 60, 10000);
+      
+      buzzerFreq = map(buzzerVal, 0, 1023, 60, 10000);
       buzzerPeriod = 1e6 / buzzerFreq;
-      halfPeriod = buzzerPeriod /2;
+
 
       for(int i = 0; i < 200; i++){
-        digitalWrite(buzzer, HIGH);// sound
+        digitalWrite(buzzerPin, HIGH);// sound
         delayMicroseconds(buzzerPeriod);//delay1ms
-        digitalWrite(buzzer,LOW);//not sound
+        digitalWrite(buzzerPin,LOW);//not sound
         delayMicroseconds(buzzerPeriod);//ms delay  
       }
 
@@ -109,17 +118,16 @@ void loop() {
 
 ////First Servo motor part ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  motorAngle = map(firstServoVal,0,1023,0,180);
-  myservo.write(firstServoAngle);// set rotation angle of the motor
+  firstMotorAngle = map(firstServoVal,0,1023,0,180);
+  myFirstServo.write(firstServoAngle);// set rotation angle of the motor
   Serial.print("1st Servor Angle ");
   Serial.println(firstServoAngle);
 
 ///Second Servor motor part//////////////////////////////////////////////////////////////////////////
 
-  motorAngle = map(secondServoVal,0,1023,0,180);
-  myservo.write(secondServoAngle);// set rotation angle of the motor
+  secondMotorAngle = map(secondServoVal,0,1023,0,180);
+  mySecondServo.write(secondServoAngle);// set rotation angle of the motor
   Serial.print("2nd Servor Angle ");
   Serial.println(secondServoAngle);
 
 }
-
